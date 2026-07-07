d75d96ba54c7aefcbdd7bc67ec0e5461c4bd3295
Luan Guimarães <141138926+luang93@users.noreply.github.com>
2026-07-06 22:34:57 -0300

    Add mod entry point

---
+++ Source/WorldConquest/WorldConquestMod.cs
@@ -0,0 +1,12 @@
+using Verse;
+
+namespace WorldConquest
+{
+    public class WorldConquestMod : Mod
+    {
+        public WorldConquestMod(ModContentPack content) : base(content)
+        {
+            Log.Message("[World Conquest] Mod carregado.");
+        }
+    }
+}
